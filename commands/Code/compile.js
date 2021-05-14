334abe04be77b986de21cc51aa70cd3e248ee6d8
Kanishq Kancharla <Kanishq0106@gmail.com>
2021-05-14 12:11:43 -0400

    fixed perms, proper data, language bugs

--- commands/Code/compile.js
+++ commands/Code/compile.js
@@ -25,12 +25,13 @@ module.exports = {
       code.trim(); 
       // console.log(`Lang Index: ${index}`);
       // console.log(`Code: \n${code}`);
-
+      code = code.replace(/\n/gi, '');
       const body = {
         "source_code": code,
         "language_id": index,
       }
       console.log(body);
+      message.channel.send("Thinking...");
       const POSToptions = {
         method: 'POST',
         url: 'https://judge0-ce.p.rapidapi.com/submissions',
@@ -53,14 +54,14 @@ module.exports = {
         message.channel.send(new MessageEmbed()
           .setColor(ee.color)
           .setFooter(ee.footertext, ee.footericon)
-          .setTitle(`Success!`)
+          .setTitle(`Compiling code...`)
           .setDescription(`Your token: \`\`\`${token}\`\`\``)
           .addField("Requested by ", user, false)
         ).then(message => {messageID = message.id})
         const GEToptions = {
           method: 'GET',
           url: 'https://judge0-ce.p.rapidapi.com/submissions/' + token,
-          qs: {base64_encoded: 'true'},
+          qs: {base64_encoded: 'true', fields: '*'},
           headers: {
             'x-rapidapi-key': 'd6f38a8010msh80def6bcd5ffaeap11ccd2jsnb191fc4ab056',
             'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
@@ -70,18 +71,32 @@ module.exports = {
         
         request(GEToptions, function (error, response, body) {
           if (error) throw new Error(error);
-        
-          console.log("GOT: "+body);
+          try{
+            body = JSON.parse(body);
+          console.log(body)
+          console.log("Output: " + body['stdout']);
+          console.log("Time: " + body['time']);
+          console.log("Status: " + body['status']['description']);
+          console.log("Language: " + body['language']['name']);
           return message.channel.send(new MessageEmbed()
             .setColor(ee.color)
             .setFooter(ee.footertext, ee.footericon)
             .setTitle("Success!")
             .setDescription(`\`\`\`${code}\`\`\``)
-            .addField("Output", body['output'], false)
-            .addField("Status: ", body['status'])
+            .addField("Output", body['stdout'], false)
+            .addField("Status: ", body['status']['description'])
             .addField("Time: ", `${body['time']}ms`)
-            .addField("Ran in ", body['language'])
+            .addField("Ran in ", body['language']['name'])
           )
+          }
+          catch(e){
+            return message.channel.send(new MessageEmbed()
+              .setColor(ee.wrongcolor)
+              .setFooter(ee.footertext, ee.footericon)
+              .setTitle(`❌ ERROR | An error occurred`)
+              .setDescription(`\`\`\`${e.stack}\`\`\``)
+            );
+          }no
         });
       });
 
